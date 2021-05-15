108098e2d40e5e5561116ba016ed3bccd7835715
Diogo Matias <diogo-mathias@live.com>
2021-05-15 10:24:05 -0300

    Barchart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,7 +11,7 @@ type CharData = {
 
 const DonutChart = () => {
 
-    const [charData, setCharData] = useState<CharData>({ labels: [], series: [] });
+    const [charData, setChartData] = useState<CharData>({ labels: [], series: [] });
 
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
@@ -20,14 +20,9 @@ const DonutChart = () => {
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
 
-                setCharData({ labels: myLabels, series: mySeries });
+                setChartData({ labels: myLabels, series: mySeries });
             });
     }, []);
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
